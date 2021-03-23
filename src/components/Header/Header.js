1efdb01a05eccfeb6bbbc73cbd813b97c2727c5a
ArcanumLibella <fiona.roux@hetic.net>
2021-03-23 19:23:56 +0100

    feat: add event handler onChange

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <header className='p-4 bg-yellow-500'>
+    <header className='p-4 bg-red-400'>
       <span className='font-black'>The Pix Finder</span>
     </header>
   )
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
-  state = { texte: ''}
+  state = { term: ''}
 
   render() {
     return (
       <div className='searchBar bg-yellow-300 p-8 rounded-b-3xl fixed w-full'>
         <form className='max-w-screen-md flex flex-col items-start mx-auto'>
           <label className='font-bold mb-2'>Search a word</label>
-          <input className='rounded w-full p-2' type='text' placeholder='cat' />
+          <input 
+            className='rounded w-full p-2' 
+            type='text' 
+            placeholder='cat'
+            value={this.state.term}
+            onChange={(e) => this.setState({term: e.target.value})}
+          />
         </form>
       </div>
     )
