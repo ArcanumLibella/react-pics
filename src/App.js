8a5f36e54092ea16618b533bbd23f479499f5141
ArcanumLibella <fiona.roux@hetic.net>
2021-03-27 16:14:52 +0100

    feat: add async request

--- src/App.js
+++ src/App.js
@@ -5,14 +5,31 @@ import Header from './components/Header/Header'
 import SearchBar from './components/SearchBar/SearchBar'
 
 class App extends React.Component {
-  handleSubmit (term) {
-    axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: term },
-      headers: {
-        Authorization: 'Client-ID FCrxCm6cMo4aLLjbtCG9mbB6Gu4j0AdO3bnBr-Tf3EE'
-      }
-    })
+  // WITH ASYNC AWAIT
+  async handleSubmit (term) {
+    const response = await axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization: 'Client-ID FCrxCm6cMo4aLLjbtCG9mbB6Gu4j0AdO3bnBr-Tf3EE'
+        }
+      })
+
+    console.log(response.data.results)
   }
+
+  // WITH .then
+  // handleSubmit (term) {
+  //   axios
+  //     .get('https://api.unsplash.com/search/photos', {
+  //       params: { query: term },
+  //       headers: {
+  //         Authorization: 'Client-ID FCrxCm6cMo4aLLjbtCG9mbB6Gu4j0AdO3bnBr-Tf3EE'
+  //       }
+  //     }).then(response => {
+  //       console.log(response.data.results)
+  //     })
+  // }
 
   render () {
     return (
