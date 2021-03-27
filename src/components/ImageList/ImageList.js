deda0bcd60e100fb4be3730a32cd3af39256fd39
ArcanumLibella <fiona.roux@hetic.net>
2021-03-27 17:50:20 +0100

    feat: add classname

--- src/components/ImageList/ImageList.js
+++ src/components/ImageList/ImageList.js
@@ -1,9 +1,9 @@
 import React, { Fragment } from 'react'
 
 const ImageList = (props) => {
-  const images = props.images.map((image, id) => {
+  const images = props.images.map((image) => {
     return (
-      <div key={image.id}>
+      <div className='image' key={image.id}>
         <img src={image.urls.regular} alt={image.alt_description} className='p-4' />
         <p className='font-semibold mb-8 uppercase'>{image.alt_description}</p>
       </div>
